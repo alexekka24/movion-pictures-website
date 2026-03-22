a9f9a9ee839be4ad6296ac564b96c703bbc64567
alexekka24 <alexekka694@gmail.com>
2026-03-22 18:11:55 +0530

    commit4

--- public/assets/data/CLIENTS.js
+++ public/assets/data/CLIENTS.js
@@ -2,7 +2,7 @@ export const CLIENTS = [
   "/assets/images/clients/addverb.webp",
   "/assets/images/clients/campusfund.webp",
   "/assets/images/clients/digantara.webp",
-  "/assets/images/clients/dotAndkey.webp",
+  "/assets/images/clients/dotandkey.webp",
   "/assets/images/clients/etherealx.webp",
   "/assets/images/clients/indian_army.webp",
   "/assets/images/clients/jm_financial.webp",
