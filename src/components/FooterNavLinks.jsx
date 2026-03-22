54601f9799564babeaaf8dc9b7dda432c8e6ed61
alexekka24 <alexekka694@gmail.com>
2026-03-22 19:38:09 +0530

    changes

--- src/components/FooterNavLinks.jsx
+++ src/components/FooterNavLinks.jsx
@@ -4,35 +4,36 @@ import Button from "./common/Button";
 import { NAVIGATION } from "../../public/assets/data/NAVIGATION";
 
 export const FooterNavLinks = ({ onContactClick }) => {
-  
+
+  const navItems = NAVIGATION.navItems.slice(0, 3);
+
   return (
     <ul className="space-y-3">
-      {NAVIGATION.navItems.map((item) => {
+      {navItems.map((item) => {
         const Icon = item.iconName;
 
         // Contact → OPEN OVERLAY
-            if (item.action === "overlay") {
-              return (
-                <a className="flex" key={item.name}>
-                  <Button
-                    key={item.name}
-                    // onClick={handleContactOpen}
-                    className="text-white hover:text-white/60 transition-colors"
-                    variant="simple"
-                    size="ex_md"
-                    text={item.name.toUpperCase()}
-                  ></Button>
-                </a>
-              );
-            }
+        if (item.action === "overlay") {
+          return (
+            <a className="flex" key={item.name}>
+              <Button
+                key={item.name}
+                // onClick={handleContactOpen}
+                className="text-white hover:text-white/60 transition-colors"
+                variant="simple"
+                size="ex_md"
+                text={item.name.toUpperCase()}
+              ></Button>
+            </a>
+          );
+        }
 
         return (
           <li key={item.name}>
             <NavLink
               to={item.href}
               className={({ isActive }) =>
-                `flex items-center gap-3 transition ${
-                  isActive ? "text-white" : "text-white/70 hover:text-white"
+                `flex items-center gap-3 transition ${isActive ? "text-white" : "text-white/70 hover:text-white"
                 }`
               }
             >
